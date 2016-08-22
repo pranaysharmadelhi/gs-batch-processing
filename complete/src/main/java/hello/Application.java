1d2bb8372e2d10e934f0d0cacfc9dc81bd57ab18
Pranay Sharma <pranaysharma@MacBookPro-C02S22CHG8WL.local>
2016-08-22 14:17:14 -0400

    cloud foundry ready

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -7,6 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
     public static void main(String[] args) throws Exception {
+        System.out.println("Running main.");
         SpringApplication.run(Application.class, args);
+        Object obj = new Object();
+        synchronized (obj) {
+            obj.wait();
+        }
     }
 }
